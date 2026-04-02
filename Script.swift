5c4d4f289b60434f46ec0184c81e3d5225ecde9d
AdminTunDeveloped <tunmediasocialmediaservices@gmail.com>
2026-04-02 17:45:55 +0700

    Update Script.swift

--- Script.swift
+++ Script.swift
@@ -3,11 +3,11 @@ func createProxySession() -> URLSession {
     
     config.connectionProxyDictionary = [
         kCFNetworkProxiesHTTPEnable as String: true,
-        kCFNetworkProxiesHTTPProxy as String: "https://raw.githubusercontent.com/AdminTunDeveloped/TunDevelopedScript/main/Script.swift",
+        kCFNetworkProxiesHTTPProxy as String: "https://raw.githubusercontent.com/AdminTunDeveloped/Data5001403/main/Data5001403.cs",
         kCFNetworkProxiesHTTPPort as String: 8080,
         
         kCFNetworkProxiesHTTPSEnable as String: true,
-        kCFNetworkProxiesHTTPSProxy as String: "https://raw.githubusercontent.com/AdminTunDeveloped/TunDevelopedScript/main/Script.swift",
+        kCFNetworkProxiesHTTPSProxy as String: "https://raw.githubusercontent.com/AdminTunDeveloped/Data5001403/main/Data5001403.csmain/Script.swift",
         kCFNetworkProxiesHTTPSPort as String: 8080
     ]
     
